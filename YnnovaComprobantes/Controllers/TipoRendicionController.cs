30e9f1aed1045e111eb7bbef2b9b2e040a9a8563
¨Sandro¨ <rubustusrubustus241@gmail.com>
2025-12-16 08:47:51 -0500

    Tiporendi y tipo usu

---
+++ YnnovaComprobantes/Controllers/TipoRendicionController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YnnovaComprobantes.Data;
+using YnnovaComprobantes.Models;
+
+namespace YnnovaComprobantes.Controllers
+{
+    public class TipoRendicionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public TipoRendicionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // LISTAR
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetTipoRendicionData()
+        {
+            try
+            {
+                var TipoRendicionData = _context.TipoRendiciones.ToList();
+                return Json(new { data = TipoRendicionData, message = "Tipos de rendicion retornados exitosamente.", status = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new ApiResponse { data = null, message = ex.Message, status = false });
+            }
+        }
+
+        // REGISTRAR
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult RegistrarTipoRendicion(TipoRendicion TipoRendicion)
+        {
+            try
+            {
+                if (_context.Empresas.Any(e => e.Ruc == TipoRendicion.Codigo))
+                {
+                    return Json(new ApiResponse { data = null, message = "Ya existe un tipo rendicion registrado con el código ingresado.", status = false });
+                }
+
+                _context.TipoRendiciones.Add(TipoRendicion);
+                _context.SaveChanges();
+                return Json(new ApiResponse { data = null, message = "Tipo de rendicion registrado exitosamente.", status = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new ApiResponse { data = null, message = ex.Message, status = false });
+            }
+        }
+        // VER
+        public IActionResult Ver(int id)
+        {
+            var TipoRendicion = _context.TipoRendiciones.FirstOrDefault(e => e.Id == id);
+            if (TipoRendicion == null)
+            {
+                return NotFound();
+            }
+            return View(TipoRendicion);
+        }
+        // EDITAR
+        public IActionResult Editar(int id)
+        {
+            var TipoRendicion = _context.TipoRendiciones.FirstOrDefault(e => e.Id == id);
+            if (TipoRendicion == null)
+            {
+                return NotFound();
+            }
+            return View(TipoRendicion);
+        }
+        [HttpPost]
+        public JsonResult EditarTipoRendicion(TipoRendicion TipoRendicion)
+        {
+            try
+            {
+                if (!_context.TipoRendiciones.Any(e => e.Id == TipoRendicion.Id))
+                {
+                    return Json(new ApiResponse { data = null, message = "El tipo de rendicion que intenta editar no existe.", status = false });
+                }
+
+                if (_context.Empresas.Where(e => e.Id != TipoRendicion.Id).Any(e => e.Ruc == TipoRendicion.Codigo))
+                {
+                    return Json(new ApiResponse { data = null, message = "Ya existe un tipo de rendicion registrado con el código ingresado.", status = false });
+                }
+
+                _context.TipoRendiciones.Update(TipoRendicion);
+                _context.SaveChanges();
+                return Json(new ApiResponse { data = TipoRendicion, message = "Tipo de rendicion actualizado exitosamente.", status = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new ApiResponse { data = null, message = ex.Message, status = false });
+            }
+        }
+        // ELIMINAR
+        public JsonResult EliminarTipoRendicion(int id)
+        {
+            try
+            {
+                var TipoRendicion = _context.TipoRendiciones.FirstOrDefault(e => e.Id == id);
+                if (TipoRendicion == null)
+                {
+                    return Json(new ApiResponse { data = null, message = "Tipo de rendicion no encontrada.", status = false });
+                }
+                _context.TipoRendiciones.Remove(TipoRendicion);
+                _context.SaveChanges();
+                return Json(new ApiResponse { data = null, message = "Tipo de rendicion eliminada exitosamente.", status = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new ApiResponse { data = null, message = ex.Message, status = false });
+            }
+        }
+
+
+    }
+}
--- YnnovaComprobantes/Controllers/TipoUsuarioController.cs
+++ YnnovaComprobantes/Controllers/TipoUsuarioController.cs
@@ -1,83 +1,125 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using YnnovaComprobantes.Data;
+using YnnovaComprobantes.Models;
 
 namespace YnnovaComprobantes.Controllers
 {
-    public class TipoUsuarioController : Controller
-    {
-        // GET: TipoUsuarioController
-        public ActionResult Index()
-        {
-            return View();
-        }
+    
 
-        // GET: TipoUsuarioController/Details/5
-        public ActionResult Details(int id)
+        //[Authorize]
+        public class TipoUsuarioController : Controller
         {
-            return View();
-        }
-
-        // GET: TipoUsuarioController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: TipoUsuarioController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
+            private readonly ApplicationDbContext _context;
+            public TipoUsuarioController(ApplicationDbContext context)
             {
-                return RedirectToAction(nameof(Index));
+                _context = context;
             }
-            catch
+            // LISTAR
+            public IActionResult Index()
             {
                 return View();
             }
-        }
-
-        // GET: TipoUsuarioController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: TipoUsuarioController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
+            public JsonResult GetTipoUsuarioData()
             {
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    var TipoUsuarioData = _context.TipoUsuarios.ToList();
+                    return Json(new { data = TipoUsuarioData, message = "Usuarios retornadas exitosamente.", status = true });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new ApiResponse { data = null, message = ex.Message, status = false });
+                }
             }
-            catch
+
+            // REGISTRAR
+            public IActionResult Registrar()
             {
                 return View();
             }
+            [HttpPost]
+            public JsonResult RegistrarTipoUsuario(TipoUsuario TipoUsuario)
+            {
+                try
+                {
+                    if (_context.TipoUsuarios.Any(e => e.Id == TipoUsuario.Id))
+                    {
+                        return Json(new ApiResponse { data = null, message = "Ya existe una Tipo Usuario registrada con el código de Tipo Usuario ingresado.", status = false });
+                    }
+
+                    _context.TipoUsuarios.Add(TipoUsuario);
+                    _context.SaveChanges();
+                    return Json(new ApiResponse { data = null, message = "Tipo Usuarioregistrada exitosamente.", status = true });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new ApiResponse { data = null, message = ex.Message, status = false });
+                }
+            }
+            // VER
+            public IActionResult Ver(int id)
+            {
+                var TipoUsuario = _context.TipoUsuarios.FirstOrDefault(e => e.Id == id);
+                if (TipoUsuario == null)
+                {
+                    return NotFound();
+                }
+                return View(TipoUsuario);
+            }
+            // EDITAR
+            public IActionResult Editar(int id)
+            {
+                var TipoUsuario = _context.TipoUsuarios.FirstOrDefault(e => e.Id == id);
+                if (TipoUsuario == null)
+                {
+                    return NotFound();
+                }
+                return View(TipoUsuario);
+            }
+            [HttpPost]
+            public JsonResult EditarTipoUsuario(TipoUsuario TipoUsuario)
+            {
+                try
+                {
+                    if (!_context.TipoUsuarios.Any(e => e.Id == TipoUsuario.Id))
+                    {
+                        return Json(new ApiResponse { data = null, message = "La Tipo Usuario que intenta editar no existe.", status = false });
+                    }
+
+                    if (_context.TipoUsuarios.Where(e => e.Id != TipoUsuario.Id).Any(e => e.Id == TipoUsuario.Id))
+                    {
+                        return Json(new ApiResponse { data = null, message = "Ya existe una Tipo Usuario registrada con el código de Tipo Usuario ingresado.", status = false });
+                    }
+
+                    _context.TipoUsuarios.Update(TipoUsuario);
+                    _context.SaveChanges();
+                    return Json(new ApiResponse { data = TipoUsuario, message = "Tipo Usuario actualizada exitosamente.", status = true });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new ApiResponse { data = null, message = ex.Message, status = false });
+                }
+            }
+            // ELIMINAR
+            public JsonResult EliminarTipoUsuario(int id)
+            {
+                try
+                {
+                    var TipoUsuario = _context.TipoUsuarios.FirstOrDefault(e => e.Id == id);
+                    if (TipoUsuario == null)
+                    {
+                        return Json(new ApiResponse { data = null, message = "Tipo Usuario no encontrada.", status = false });
+                    }
+                    _context.TipoUsuarios.Remove(TipoUsuario);
+                    _context.SaveChanges();
+                    return Json(new ApiResponse { data = null, message = "Tipo Usuario eliminada exitosamente.", status = true });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new ApiResponse { data = null, message = ex.Message, status = false });
+                }
+            }
         }
 
-        // GET: TipoUsuarioController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: TipoUsuarioController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
-}
